de74f4eab88d562ecc6d7d2ded2d1376bfa38700
Murilo Ferraz Franco <muriloffranco@gmail.com>
2016-04-22 13:28:26 -0300

    Versão inicial do controlador de acesso com projeto alterado, resolvendo erros de compilação.

---
+++ src/main/java/br/ufg/inf/fabrica/pac/seguranca/ILdapAutenticador.java
@@ -0,0 +1,17 @@
+package br.ufg.inf.fabrica.pac.seguranca;
+
+
+
+
+/**
+ *
+ * @author danilloguimaraes
+ */
+public interface ILdapAutenticador {
+
+    /**
+     * 
+     * @param usuario 
+     */
+    //public Usuario autenticar(Usuario usuario);
+}
